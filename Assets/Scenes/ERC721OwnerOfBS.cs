caf282f33f5edbbf4724b11d333925f093a5b494
Dave Gámez <me@davegamez.com>
2023-09-25 19:40:58 -0600

    build.framework.js:2784 Exception: could not detect network   at Web3Unity.Scripts.Library.Ethers.Providers.JsonRpcProvider._uncachedDetectNetwork () [0x00000] in <00000000000000000000000000000000>:0

---
+++ Assets/Scenes/ERC721OwnerOfBS.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Web3Unity.Scripts.Library.ETHEREUEM.EIP;
+
+public class ERC721OwnerOfBS : MonoBehaviour
+{
+    async void Start()
+    {
+        string contract = "0x2934f6e18b11eb55eA4aA7AFA3e24A6cEC95c17C";
+        string tokenId = "1725";
+        string ownerOf = await ERC721.OwnerOf(contract, tokenId);
+        print(ownerOf);
+    }
+}
